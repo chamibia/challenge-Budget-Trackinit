b33d8d00e30b0bbb9e3bd9c7272f011a46930164
chamibia <b.chami39@gmail.com>
2022-03-14 12:50:05 -0400

    added idb.js script tag, and cdn links to serviceworker

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,8 @@ const FILES_TO_CACHE = [
     "./icons/icon-144x144.png",
     "./icons/icon-152x152.png",
     "./icons/icon-192x192.png",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
     "./icons/icon-384x384.png",
     "./icons/icon-512x512.png",
     "./js/index.js",
